09bf6c8129f73f57e293c93d1a9f2aa17eda946a
suvan <xuwan.j@gmail.com>
2018-03-21 00:36:30 +0800

    Java多线程技能

--- src/main/java/com/suvan/Thread/TestFirstDemo.java
+++ src/main/java/com/suvan/Thread/TestFirstDemo.java
@@ -18,17 +18,29 @@ public class TestFirstDemo extends Thread{
     }
 
     public static void main(String[] args) {
-        MyThread myThread = new MyThread();
-        Thread t1 = new Thread(myThread);
-        Thread t2 = new Thread(myThread);
-        Thread t3 = new Thread(myThread);
-        Thread t4 = new Thread(myThread);
-        Thread t5 = new Thread(myThread);
-        t1.start();
-        t2.start();
-        t3.start();
-        t4.start();
-        t5.start();
+        /*try {
+            MyThread1 myThread1 = new MyThread1();
+            myThread1.start();
+            myThread1.interrupt();
+            System.out.println("是否停止1？ = " + Thread.interrupted());
+            System.out.println("是否停止2？ = " + Thread.interrupted());
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("end");*/
+        /*Thread.currentThread().interrupt();
+        System.out.println("是否停止1？ = " + Thread.interrupted());
+        System.out.println("是否停止2？ = " + Thread.interrupted());*/
+        MyThread1 myThread1 = new MyThread1();
+        myThread1.setDaemon(true);
+        myThread1.start();
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("离开了");
     }
 }
 
@@ -46,5 +58,14 @@ class MyThread extends Thread {
     public void run() {
         super.run();
         System.out.println("i=" + i-- + " threadName=" + Thread.currentThread().getName());
+    }
+}
+
+class MyThread1 extends Thread {
+    @Override
+    public void run() {
+        for (int i = 0; i < 500000; i++) {
+            System.out.println("i=" + (i + 1));
+        }
     }
 }
